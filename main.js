dd3e08f2e40b7004961b21ae08ffa549d46d2533
CalvinLi <cli166@ucsc.edu>
2025-01-11 17:46:43 -0800

    Final commit

--- main.js
+++ main.js
@@ -29,8 +29,10 @@ class Player {
     }
 
     printPlayer() {
-        console.log(this.name);
-        console.log(this.attributes);
+        console.log(`NAME: ${this.name}`);
+        for (const [key, value] of Object.entries(this.attributes)) {
+            console.log(`${key.slice(0, 3).toUpperCase()}: ${value}`);
+        }
     }
 }
 
